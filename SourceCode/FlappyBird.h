1db1b0d527b694edeaa2af4e06a419e5cf1f590d
duongdefender <thuyduong8055@gmail.com>
2024-05-07 22:40:34 +0700

    fix bug snakewind, flappybird

--- SourceCode/FlappyBird.h
+++ SourceCode/FlappyBird.h
@@ -2,21 +2,21 @@ int yBird;
 
 int heightArray[31][2];
 
-void drawLeftSewer(int x, int h) {
+void drawLeftSewer(int x, int h) { // vẽ ống cống trái
   tft.fillRect(x * 8 + 4, 0, 4, (h - 2) * 8, green);
   tft.fillRect(x * 8, (h - 2) * 8, 8, 16, green);
   tft.fillRect(x * 8, (h + 9) * 8, 8, 16, green);
   tft.fillRect(x * 8 + 4, (h + 11) * 8, 4, (19 - h) * 8, green);
 }
 
-void drawMidSewer(int x, int h) {
+void drawMidSewer(int x, int h) { // vẽ ống cống giữa
   tft.fillRect(x * 8, 0, 8, (h - 2) * 8, green);
   tft.fillRect(x * 8, (h - 2) * 8, 8, 16, green);
   tft.fillRect(x * 8, (h + 9) * 8, 8, 16, green);
   tft.fillRect(x * 8, (h + 11) * 8, 8, (19 - h) * 8, green);
 }
 
-void drawRightSewer(int x, int h) {
+void drawRightSewer(int x, int h) { // vẽ ống cống phải
   tft.fillRect(x * 8 + 4, 0, 4, (h - 2) * 8, black);
   tft.fillRect(x * 8 + 4, (h + 11) * 8, 4, (19 - h) * 8, black);
   tft.fillRect(x * 8, 0, 4, (h - 2) * 8, green);
@@ -25,39 +25,39 @@ void drawRightSewer(int x, int h) {
   tft.fillRect(x * 8, (h + 11) * 8, 4, (19 - h) * 8, green);
 }
 
-void drawSewer(int x, int h) {
+void drawSewer(int x, int h) { // vẽ ống cống
   tft.fillRect(x * 8 + 4, 0, 16, (h - 2) * 8, green);
   tft.fillRect(x * 8, (h - 2) * 8, 24, 16, green);
   tft.fillRect(x * 8, (h + 9) * 8, 24, 16, green);
   tft.fillRect(x * 8 + 4, (h + 11) * 8, 16, (19 - h) * 8, green);
 }
 
-void drawRunSewer() {
+void drawRunSewer() { // vẽ ống cống mới
   for (int i = 0; i < 30; ++i) {
     if (heightArray[i][0]) {
       switch (heightArray[i][1]) {
-        case 0:
-          heightArray[i][1] = 1;
-          drawMidSewer(i, heightArray[i][0]);
+        case 0: // ống cống hiện tại là trái 
+          heightArray[i][1] = 1; 
+          drawMidSewer(i, heightArray[i][0]); // vẽ ống cống giữa
           break;
-        case 1:
-          heightArray[i][1] = 2;
-          drawRightSewer(i, heightArray[i][0]);
+        case 1: // ống cống hiện tại là giữa
+          heightArray[i][1] = 2; 
+          drawRightSewer(i, heightArray[i][0]); // vẽ ống cống phải
           break;
-        default:
+        default: // ống cống hiện tải là phải
           tft.fillRect(i * 8, 0, 8, heightArray[i][0] * 8, black);
           tft.fillRect(i * 8, (heightArray[i][0] + 9) * 8, 8, (21 - heightArray[i][0]) * 8, black);
-          heightArray[i][0] = 0;
+          heightArray[i][0] = 0; // xóa ống cống hiện tại
       }
-    } else if (heightArray[i + 1][0]) {
-      heightArray[i][0] = heightArray[i + 1][0];
+    } else if (heightArray[i + 1][0]) { // không có ống cống thì kiểm tra vị trí tiếp theo
+      heightArray[i][0] = heightArray[i + 1][0]; // sao chép chiều cao ống tiếp theo về ống hiện tại
       heightArray[i][1] = 0;
-      drawLeftSewer(i, heightArray[i][0]);
+      drawLeftSewer(i, heightArray[i][0]); // vẽ ống cống trái
     }
   }
 }
 
-void drawBird() {
+void drawBird() { // vẽ chim
   tft.fillRoundRect(32, yBird * 8, 24, 24, 5, yellow);
   tft.fillRoundRect(44, yBird * 8, 12, 12, 5, white);
   tft.fillRoundRect(24, yBird * 8 + 8, 16, 16, 5, white);
@@ -66,22 +66,22 @@ void drawBird() {
 }
 
 bool checkGameOverFB() {
-  if (heightArray[4][0]) {
-    if (heightArray[4][0] >= yBird || yBird >= heightArray[4][0] + 6) {
+  if (heightArray[4][0]) { 
+    if (heightArray[4][0] >= yBird || yBird >= heightArray[4][0] + 6) { // nếu đụng phải ống cống gameover
       if (commonScore > bestFlappyBird) {
         bestFlappyBird = commonScore;
         EEPROM.write(0, commonScore);
-        EEPROM.commit();
+        EEPROM.commit(); // lưu điểm kỷ lục
       }
       return true;
     } 
   }
   if (heightArray[6][0]) {
-    if (heightArray[6][0] >= yBird || yBird >= heightArray[6][0] + 6) {
+    if (heightArray[6][0] >= yBird || yBird >= heightArray[6][0] + 6) { // nếu đụng phải ống cống gameover
       if (commonScore > bestFlappyBird) {
         bestFlappyBird = commonScore;
         EEPROM.write(0, commonScore);
-        EEPROM.commit();
+        EEPROM.commit(); // lưu điểm kỷ lục
       }
       return true;
     }
@@ -89,38 +89,38 @@ bool checkGameOverFB() {
   return false;
 }
 
-void birdFly() {
-  if (!digitalRead(top)) {
+void birdFly() { 
+  if (!digitalRead(top)) { // chim bay lên khi nhấn nút trên
     tft.fillRect(32, yBird * 8, 24, 24, black);
     tft.fillRect(24, yBird * 8 + 8, 8, 16, black);
     tft.fillRect(56, yBird * 8 + 16, 8, 4, black);
-    --yBird;
+    --yBird; 
     drawBird();
   }
 }
 
-void animationFB() {
+void animationFB() { // hoạt ảnh chim bay qua ống
   drawRunSewer();
   tft.fillRect(32, yBird * 8, 24, 24, black);
   tft.fillRect(24, yBird * 8 + 8, 8, 16, black);
   tft.fillRect(56, yBird * 8 + 16, 8, 4, black);
-  ++yBird;
-  drawBird();
-  if (heightArray[3][0] && !heightArray[4][0]) increaseScore();
+  ++yBird; 
+  drawBird(); 
+  if (heightArray[3][0] && !heightArray[4][0]) increaseScore(); // bay qua ống cống tăng điểm
 }
 
-int newGameFlappyBird() {
-  drawSubScreen(bestFlappyBird, 0);
+int newGameFlappyBird() { // chơi lại
+  drawSubScreen(bestFlappyBird, 0); // hiển thị điểm cao nhất
 
-  memset(heightArray, 0, sizeof(heightArray));
+  memset(heightArray, 0, sizeof(heightArray)); 
 
-  for (int i = 12; i < 30; i += 6) {
-    int rd = random(7, 15);
+  for (int i = 12; i < 30; i += 6) { // vẽ ống cách nhau 6 vị trí
+    int rd = random(7, 15); // random chiều cao ống
     heightArray[i][0] = heightArray[i + 1][0] = heightArray[i + 2][0] = rd;
     heightArray[i][1] = 0;
     heightArray[i + 1][1] = 1;
-    heightArray[i + 2][1] = 2;
-    drawSewer(i, rd);
+    heightArray[i + 2][1] = 2; // 3 ống lần lượt trái, giữa , phải
+    drawSewer(i, rd); // vẽ 3 ống
   }
 
   yBird = 15;
@@ -130,7 +130,7 @@ int newGameFlappyBird() {
     int rd = random(7, 15);
 
     heightArray[30][0] = rd;
-    heightArray[30][1] = 0;
+    heightArray[30][1] = 0; // vẽ ống trái
     for (int i = 0; i < 5; ++i) {
       if (!digitalRead(enterPause)) return 0;
       birdFly();
@@ -141,7 +141,7 @@ int newGameFlappyBird() {
     if (checkGameOverFB()) return 1;
 
     heightArray[30][0] = rd;
-    heightArray[30][1] = 1;
+    heightArray[30][1] = 1; // vẽ ống giữa
     for (int i = 0; i < 5; ++i) {
       if (!digitalRead(enterPause)) return 0;
       birdFly();
@@ -152,7 +152,7 @@ int newGameFlappyBird() {
     if (checkGameOverFB()) return 1;
 
     heightArray[30][0] = rd;
-    heightArray[30][1] = 2;
+    heightArray[30][1] = 2; // vẽ ống phải
     for (int i = 0; i < 5; ++i) {
       if (!digitalRead(enterPause)) return 0;
       birdFly();
@@ -162,7 +162,7 @@ int newGameFlappyBird() {
     animationFB();
     if (checkGameOverFB()) return 1;
 
-    heightArray[30][0] = 0;
+    heightArray[30][0] = 0; // vẽ khoảng trống
     for (int j = 0; j < 3; ++j) {
       for (int i = 0; i < 5; ++i) {
         if (!digitalRead(enterPause)) return 0;
--- SourceCode/SnakeWind.h
+++ SourceCode/SnakeWind.h
@@ -4,97 +4,97 @@ std::vector<coordinates> snake;
 
 coordinates food, bigFood;
 
-void drawSnakeHead() {
+void drawSnakeHead() { // vẽ đầu rắn
   tft.fillRoundRect(snake[0].x * 8, snake[0].y * 8, 8, 8, 1, yellow);
   tft.fillRoundRect(snake[0].x * 8 + 3, snake[0].y * 8 + 2, 3, 3, 1, black);
 }
 
-void drawPartSnakeBody(int x, int y) {
+void drawPartSnakeBody(int x, int y) { // vẽ thân rắn
   tft.fillRoundRect(x * 8, y * 8, 8, 8, 1, green);
 }
 
 void generateFood() {
-  while (1) {
-    int x = random(30), y = random(30);
-    bool b = false;
+  while (1) { // tạo thức ăn mới khi hết thức ăn cũ
+    int x = random(30), y = random(30); // random tọa độ thức ăn mới
+    bool b = false; // kiểm tra xem tọa độ có trùng với vị trí rắn không
     for (int i = 0; i < snake.size(); ++i) {
       if (snake[i].x == x && snake[i].y == y) {
-        b = true;
+        b = true; // trùng tọa độ với rắn
         break;
       }
     }
-    int xx = bigFood.x, yy = bigFood.y;
-    if ((x == xx && y == yy) || (x == xx + 1 && y == yy) || (x == xx && y == yy + 1) || (x == xx + 1 && y == yy + 1)) b = true;
-    if (b) continue;
+    int xx = bigFood.x, yy = bigFood.y; // tiếp tục kiểm tra có trùng tọa độ với thức ăn to không
+    if ((x == xx && y == yy) || (x == xx + 1 && y == yy) || (x == xx && y == yy + 1) || (x == xx + 1 && y == yy + 1)) b = true; 
+    if (b) continue; // nếu vẫn trùng gán tọa độ mới
     food.x = x;
     food.y = y;
-    tft.fillRoundRect(x * 8, y * 8, 8, 8, 3, white);
+    tft.fillRoundRect(x * 8, y * 8, 8, 8, 3, white); // vẽ thức ăn ra màn hình
     return;
   }
 }
 
 void generateBigFood() {
   while (1) {
-    int x = random(29), y = random(29);
-    bool b = false;
+    int x = random(29), y = random(29); // random tọa độ 
+    bool b = false; // đặt biến kiểm tra có trùng tọa độ rắn không
     for (int i = 0; i < snake.size(); ++i) {
       int xx = snake[i].x, yy = snake[i].y;
       if ((xx == x && yy == y) || (xx == x + 1 && yy == y) || (xx == x && yy == y + 1) || (xx == x + 1 && yy == y + 1)) {
-        b = true;
+        b = true; // trùng tọa độ với rắn
         break;
       }
       xx = food.x, yy = food.y;
       if ((xx == x && yy == y) || (xx == x + 1 && yy == y) || (xx == x && yy == y + 1) || (xx == x + 1 && yy == y + 1)) {
-        b = true;
+        b = true; // trùng tọa độ với thức ăn thường
         break;
       }
     }
-    if (b) continue;
+    if (b) continue; // nếu vẫn trùng gán tọa độ mới
     bigFood.x = x;
     bigFood.y = y;
-    tft.fillRoundRect(x * 8, y * 8, 16, 16, 5, red);
+    tft.fillRoundRect(x * 8, y * 8, 16, 16, 5, red); // vẽ thức ăn to ra màn hình
     return;
   }
 }
 
 void drawRunSnake(int x, int y) {
-  coordinates tmp = snake[snake.size() - 1];
+  coordinates [tmp = snakesnake.size() - 1];  // Lưu lại tọa độ ô cuối cùng
   int b = 0;
-  if (x == food.x && y == food.y) {
-    increaseScore();
-    b = 1;
-  } else if (bigFood.x != -1) {
+  if (x == food.x && y == food.y) { // Ăn đc thức ăn thường
+    increaseScore(); // Tăng điểm
+    b = 1; // Đánh dấu đã ăn thức ăn
+  } else if (bigFood.x != -1) {  //Ăn đc thức ăn to
     int xx = bigFood.x, yy = bigFood.y;
     if ((x == xx && y == yy) || (x == xx + 1 && y == yy) || (x == xx && y == yy + 1) || (x == xx + 1 && y == yy + 1)) {
-      commonScore += 4;
-      increaseScore();
-      b = 2;
+      commonScore += 4; // tăng 4 điểm
+      increaseScore(); // 5 điểm
+      b = 2; // Đánh dấu đã ăn thức ăn to
     }
   }
-  for (int i = snake.size() - 1; i > 0; --i) snake[i] = snake[i - 1];
-  snake[0].x = x;
+  for (int i = snake.size() - 1; i > 0; --i) snake[i] = snake[i - 1]; // Dịch chuyển vị trí các phần thân
+  snake[0].x = x; // Đặt vị trí mới cho đầu rắn
   snake[0].y = y;
-  if (b) {
-    snake.push_back(tmp);
-    if (b == 1) generateFood();
+  if (b) { //Nếu có ăn thức ăn
+    snake.push_back(tmp); //Thêm phần thân cuối cùng vào
+    if (b == 1) generateFood(); // Tạo thức ăn mới
     else {
-      tft.fillRect(bigFood.x * 8, bigFood.y * 8, 16, 16, black);
+      tft.fillRect(bigFood.x * 8, bigFood.y * 8, 16, 16, black); // Xóa thức ăn to
       bigFood.x = -1;
     }
-  } else tft.fillRect(tmp.x * 8, tmp.y * 8, 8, 8, black);
+  } else tft.fillRect(tmp.x * 8, tmp.y * 8, 8, 8, black); // xóa ô cuối cùng
   drawSnakeHead();
   drawPartSnakeBody(snake[1].x, snake[1].y);
 }
 
 bool checkGameOverSW() {
   for (int i = 1; i < snake.size(); ++i) {
-    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
-      if (commonScore > bestSnakeWind) {
-        bestSnakeWind = commonScore;          
-        EEPROM.write(3, commonScore);
-        EEPROM.commit();
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) { // Tọa độ thân trùng với đầu
+      if (commonScore > bestSnakeWind) { 
+        bestSnakeWind = commonScore;       // Nếu điểm hiện tại cao hơn kỷ lục thì ghi lại kỷ lục    
+        EEPROM.write(3, commonScore); // lưu điểm
+        EEPROM.commit(); 
       }
-      snake.clear();
+      snake.clear(); // Xóa toàn bộ rắn 
       return true;
     }
   }
@@ -102,10 +102,10 @@ bool checkGameOverSW() {
 }
 
 void moveSnake() {
-  if (!digitalRead(top) && directionSnakeWind != 2) {
-    directionSnakeWind = 0;
-    if (!snake[0].y) drawRunSnake(snake[0].x, 29);
-    else drawRunSnake(snake[0].x, snake[0].y - 1);
+  if (!digitalRead(top) && directionSnakeWind != 2) { // Nếu ấn nút lên và hướng chưa phải hướng xuống
+    directionSnakeWind = 0; // đổi hướng lên
+    if (!snake[0].y) drawRunSnake(snake[0].x, 29); // nếu đầu ở trên cùng thì xuống dưới cùng
+    else drawRunSnake(snake[0].x, snake[0].y - 1);  // di chuyển lên 1 ô
   } else if (!digitalRead(right) && directionSnakeWind != 3) {
     directionSnakeWind = 1;
     if (snake[0].x == 29) drawRunSnake(0, snake[0].y);
@@ -118,8 +118,8 @@ void moveSnake() {
     directionSnakeWind = 3;
     if (!snake[0].x) drawRunSnake(29, snake[0].y);
     else drawRunSnake(snake[0].x - 1, snake[0].y);
-  } else {
-    if (directionSnakeWind == 0) {
+  } else {  // Nếu không ấn phím
+    if (directionSnakeWind == 0) { // kiểm tra hướng hiện tại và di chuyển theo hướng đó
       if (!snake[0].y) drawRunSnake(snake[0].x, 29);
       else drawRunSnake(snake[0].x, snake[0].y - 1);
     } else if (directionSnakeWind == 1) {
@@ -135,62 +135,62 @@ void moveSnake() {
   }
 }
 
-int newGameSnakeWind() {
-  drawSubScreen(bestSnakeWind, 0);
+int newGameSnakeWind() { // chơi lại
+  drawSubScreen(bestSnakeWind, 0); // In ra màn hình kết quả tốt nhất
 
-  snake.clear();
+  snake.clear(); // tạo lại con rắn
   snake.push_back({15, 15});
   snake.push_back({14, 15});
 
-  drawSnakeHead();
+  drawSnakeHead(); // vẽ đầu và thân rắn
   drawPartSnakeBody(snake[1].x, snake[1].y);
-  generateFood();
+  generateFood(); // tạo thức ăn
 
-  directionSnakeWind = 1;
+  directionSnakeWind = 1; // hướng di chuyển mặc định
 
   while (1) {
     for (int i = 0; i < 100; ++i) {
-      if (!digitalRead(enterPause)) return 0;
+      if (!digitalRead(enterPause)) return 0; // kiểm tra có nhấn pause không
       moveSnake();
-      if (checkGameOverSW()) return 1;
+      if (checkGameOverSW()) return 1; // kiểm tra gameover
       delay(100);
     }
-    generateBigFood();
+    generateBigFood(); // tạo thức ăn to
     for (int i = 0; i < 40; ++i) {
       if (!digitalRead(enterPause)) return 0;
       moveSnake();
       if (checkGameOverSW()) return 1;
       delay(100);
     }
-    if (bigFood.x != -1) {
+    if (bigFood.x != -1) { // nếu ăn thức ăn to thì xóa thức ăn to
       tft.fillRect(bigFood.x * 8, bigFood.y * 8, 16, 16, black);
       bigFood.x = -1;
     }
   }
 }
 
-int continueSnakeWind() {
-  drawSubScreen(bestSnakeWind, commonScore);
+int continueSnakeWind() { // tiếp tục chơi
+  drawSubScreen(bestSnakeWind, commonScore); // in ra màn hình kết quả hiện tại
 
-  drawSnakeHead();
+  drawSnakeHead(); // vẽ lại rắn
   for (int i = 1; i < snake.size(); ++i) drawPartSnakeBody(snake[i].x, snake[i].y);
   tft.fillRoundRect(food.x * 8, food.y * 8, 8, 8, 3, white);
   
   while (1) {
-    for (int i = 0; i < 60; ++i) {
+    for (int i = 0; i < 100; ++i) {
+      if (!digitalRead(enterPause)) return 0; // kiểm tra có nhấn pause không
+      moveSnake();
+      if (checkGameOverSW()) return 1; // kiểm tra gameover
+      delay(100);
+    }
+    generateBigFood(); // tạo thức ăn to
+    for (int i = 0; i < 40; ++i) {
       if (!digitalRead(enterPause)) return 0;
       moveSnake();
       if (checkGameOverSW()) return 1;
-      delay(150);
+      delay(100);
     }
-    generateBigFood();
-    for (int i = 0; i < 30; ++i) {
-      if (!digitalRead(enterPause)) return 0;
-      moveSnake();
-      if (checkGameOverSW()) return 1;
-      delay(150);
-    }
-    if (bigFood.x != -1) {
+    if (bigFood.x != -1) { // nếu ăn thức ăn to thì xóa thức ăn to
       tft.fillRect(bigFood.x * 8, bigFood.y * 8, 16, 16, black);
       bigFood.x = -1;
     }
